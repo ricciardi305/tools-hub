44bf7c9e92f124e272f34d2be78a5d0d7c930fd9
Rafael Ricciardi <ricciardi.rafael1997@gmail.com>
2022-08-20 21:38:13 -0300

    documentation

--- src/app.ts
+++ src/app.ts
@@ -3,12 +3,12 @@ import express from "express";
 import swaggerUI from "swagger-ui-express";
 import { handleErrors } from "./errors/HandleErrors";
 import { useRoutes } from "./routes";
-// import swagerDocs from "./swagger.json";
+import swagerDocs from "./swagger.json";
 
 const app = express();
 
 app.use(express.json());
-// app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(swagerDocs));
+app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(swagerDocs));
 useRoutes(app);
 
 app.get("/", (req, res) => {
